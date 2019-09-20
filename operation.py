9fef442ce556c9471741f3814ec30c61a4f3c33c
742362144 <742362144@qq.com>
2019-09-20 09:25:20 +0800

    add show disk support

--- operation.py
+++ operation.py
@@ -100,7 +100,7 @@ def createPool(params):
             if not os.path.isdir(POOL_PATH):
                 os.makedirs(POOL_PATH)
 
-            Operation("mount -t nfs " + params.url + POOL_PATH, {})
+            Operation("mount -t nfs " + params.url + ' ' + POOL_PATH, {})
 
             kv = {"type": "dir", "target": POOL_PATH, "name": params.pool}
             op2 = Operation("virsh pool-create-as", kv)
