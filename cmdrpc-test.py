0edd558e95748e87d71e7e160431c4c030d3154f
742362144 <742362144@qq.com>
2019-09-22 13:26:16 +0800

    add rpc support

--- cmdrpc-test.py
+++ cmdrpc-test.py
@@ -114,62 +114,7 @@ def run_server():
         server.stop(0)
 
 
-class ClientDaemon(CDaemon):
-    def __init__(self, name, save_path, stdin=os.devnull, stdout=os.devnull, stderr=os.devnull, home_dir='.', umask=022,
-                 verbose=1):
-        CDaemon.__init__(self, save_path, stdin, stdout, stderr, home_dir, umask, verbose)
-        self.name = name
-
-    @singleton('/var/run/cmdrpc.pid')
-    def run(self, output_fn, **kwargs):
-        logger.debug("---------------------------------------------------------------------------------")
-        logger.debug("------------------------Welcome to Virtlet Daemon.-------------------------------")
-        logger.debug("------Copyright (2019, ) Institute of Software, Chinese Academy of Sciences------")
-        logger.debug("---------author: wuyuewen@otcaix.iscas.ac.cn,liuhe18@otcaix.iscas.ac.cn----------")
-        logger.debug("--------------------------------wuheng@otcaix.iscas.ac.cn------------------------")
-        logger.debug("---------------------------------------------------------------------------------")
-
-        try:
-            thread_1 = Thread(target=run_server)
-            thread_1.daemon = True
-            thread_1.name = 'run_server'
-            thread_1.start()
-            try:
-                while True:
-                    time.sleep(1)
-            except KeyboardInterrupt:
-                return
-        except:
-            logger.error('Oops! ', exc_info=1)
-
-
-def daemonize():
-    help_msg = 'Usage: python %s <start|stop|restart|status>' % sys.argv[0]
-    if len(sys.argv) != 2:
-        print help_msg
-        sys.exit(1)
-    p_name = 'virtlet'
-    pid_fn = '/var/run/cmdrpc.pid'
-    log_fn = '/var/log/cmdrpc.log'
-    err_fn = '/var/log/cmdrpc.log'
-    cD = ClientDaemon(p_name, pid_fn, stderr=err_fn, verbose=1)
-
-    if sys.argv[1] == 'start':
-        cD.start(log_fn)
-    elif sys.argv[1] == 'stop':
-        cD.stop()
-    elif sys.argv[1] == 'restart':
-        cD.restart(log_fn)
-    elif sys.argv[1] == 'status':
-        alive = cD.is_running()
-        if alive:
-            print 'process [%s] is running ......' % cD.get_pid()
-        else:
-            print 'daemon process [%s] stopped' % cD.name
-    else:
-        print 'invalid argument!'
-        print help_msg
 
 
 if __name__ == '__main__':
-    daemonize()
+    run_server()
