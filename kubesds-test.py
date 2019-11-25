399f0746d5afa72f5a69a25002dab770b2fac688
742362144 <742362144@qq.com>
2019-11-25 15:28:52 +0800

    change create pool

--- kubesds-test.py
+++ kubesds-test.py
@@ -23,17 +23,17 @@ def get_cstor_pool_info(pool):
 def check_pool_type(pool, type):
     poolInfo = get_cstor_pool_info(pool)
     if type == "dir":
-        if poolInfo['result']['code'] == 0 and poolInfo['data']['proto'] != 'localfs':
+        if poolInfo['result']['code'] == 0 and poolInfo['data']['pooltype'] != 'localfs':
             print {"result": {"code": 221, "msg": "type is not match, plz check"}, "data": {}}
             exit(3)
     else:
         if poolInfo['result']['code'] == 0:  # is cstor pool, and check pool type
             # check pool type, if pool type not match, stop delete pool
-            if 'proto' not in poolInfo['data'].keys():
-                print {"result": {"code": 221, "msg": "can not get pool proto, cstor-cli cmd bug"}, "data": {}}
+            if 'pooltype' not in poolInfo['data'].keys():
+                print {"result": {"code": 221, "msg": "can not get pool pooltype, cstor-cli cmd bug"}, "data": {}}
                 exit(3)
 
-            if poolInfo['data']['proto'] != type:
+            if poolInfo['data']['pooltype'] != type:
                 print {"result": {"code": 221, "msg": "type is not match, plz check"}, "data": {}}
                 exit(3)
         else:  # not is cstor pool, exit
