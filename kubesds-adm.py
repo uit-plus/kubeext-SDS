f16a9389c495469cba7850d78d1d4815eddb53b2
742362144 <742362144@qq.com>
2019-12-11 16:04:25 +0800

    add prepare

--- kubesds-adm.py
+++ kubesds-adm.py
@@ -581,11 +581,11 @@ parser_clone_disk.set_defaults(func=cloneDiskParser)
 
 # -------------------- add prepareDisk cmd ----------------------------------
 parser_prepare_disk = subparsers.add_parser("prepareDisk", help="prepareDisk help")
-parser_prepare_disk.add_argument("--domain", required=True, metavar="[DOMAIN]", type=str,
+parser_prepare_disk.add_argument("--domain", metavar="[DOMAIN]", type=str,
                               help="storage pool to use")
-parser_prepare_disk.add_argument("--vol", required=True, metavar="[VOL]", type=str,
+parser_prepare_disk.add_argument("--vol", metavar="[VOL]", type=str,
                               help="volume name to use")
-parser_prepare_disk.add_argument("--path", required=True, metavar="[PATH]", type=str,
+parser_prepare_disk.add_argument("--path", metavar="[PATH]", type=str,
                               help="volume uni to use")
 # set default func
 parser_prepare_disk.set_defaults(func=prepareDiskParser)
