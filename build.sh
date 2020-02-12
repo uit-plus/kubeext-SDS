92e73865964761f040c6ce8a163b662ab35b4474
742362144 <742362144@qq.com>
2020-02-12 11:10:17 +0800

    add service

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 pyinstaller -F kubesds-adm.py
 pyinstaller -F kubesds-rpc.py
+pyinstaller -F kubesds-rpc-service.py
 
-chmod +x ./dist/kubesds-adm ./dist/kubesds-rpc
+chmod +x ./dist/kubesds-adm ./dist/kubesds-rpc ./dist/kubesds-rpc-service
 
-cp -f ./dist/kubesds-adm ./dist/kubesds-rpc /usr/bin
+cp -f ./dist/kubesds-adm ./dist/kubesds-rpc ./dist/kubesds-rpc-service /usr/bin
